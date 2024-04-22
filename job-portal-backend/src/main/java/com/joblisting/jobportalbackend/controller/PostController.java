c6908db874fead6632dbfc3d071bf3b20fd3f0b0
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-04-22 17:22:56 +0530

    files updated 2

---
+++ job-portal-backend/src/main/java/com/joblisting/jobportalbackend/controller/PostController.java
@@ -0,0 +1,10 @@
+package com.joblisting.jobportalbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class PostController {
+
+}
